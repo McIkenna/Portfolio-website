b0328f56043192c0ceaa441244f54dd6b7a8d45f
Ikenna Ifekaonwu <sontoly@gmail.com>
2021-09-08 23:11:05 -0400

    frontRevamped

---
+++ src/actions/ProjectActions.js
@@ -0,0 +1,92 @@
+import axios from "axios"
+import { DELETE_PROJECT, GET_ERRORS, GET_PROJECT, GET_PROJECTS, UPLOAD_TASK, GET_TASKS } from "./types";
+
+export const createProject = (project, history) => async dispatch => {
+    try {
+       await axios.post(`https://ikenna-portfolio-java.herokuapp.com/admin/project/uploadFile`, project)
+        history.push("/");
+        dispatch({
+            type: GET_ERRORS,
+            payload: {}
+        })
+    } catch (error) {
+        dispatch({
+            type: GET_ERRORS,
+            payload: error.response.data
+        })
+        
+    }
+}
+
+export const getProjects = () => async dispatch => {
+    const res = await axios.get(`https://ikenna-portfolio-java.herokuapp.com/api/project/all`)
+    dispatch({
+        type: GET_PROJECTS,
+        payload: res.data
+    })
+}
+
+export const deleteProject = id => async dispatch => {
+    if(window.confirm("This will delete Permanent")){
+        await axios.delete(`https://ikenna-portfolio-java.herokuapp.com/admin/project/${id}`)
+        dispatch({
+            type: DELETE_PROJECT,
+            payload: id
+        })
+    }
+}
+
+export const getStoredProject = (id, history) => async dispatch => {
+    try {
+        const res = await axios.get(`https://ikenna-portfolio-java.herokuapp.com/api/project/${id}`)
+        dispatch({
+            type: GET_PROJECT,
+            payload: res.data
+        })
+    } catch (error) {
+        history.push("/");
+        
+    }
+}
+/*
+export const uploadTask = (task, history) => async dispatch => {
+    try{
+        const res = await axios.post("http://localhost:8080/api/file/uploadFile", task)
+        history.push("/dashboard");
+        dispatch({
+            type: GET_ERRORS,
+            payload: {}
+        })
+
+    }catch(error){
+        dispatch({
+            type: GET_ERRORS,
+            payload: error.response.data
+        })
+    }
+}
+
+export const getStoredTasks = () => async dispatch =>{
+        const res = await axios.get("http://localhost:8080/api/file/uploads/all")
+    dispatch({
+        type: GET_TASKS,
+        payload: res.data
+    })
+}*/
+
+export const updateProject = (project, history) => async dispatch => {
+    try {
+       await axios.post(`https://ikenna-portfolio-java.herokuapp.com/admin/project/uploadFile`, project)
+        history.push("/");
+        dispatch({
+            type: GET_ERRORS,
+            payload: {}
+        })
+    } catch (error) {
+        dispatch({
+            type: GET_ERRORS,
+            payload: error.response.data
+        })
+        
+    }
+}
---
+++ src/reducers/infoReducer.js
@@ -0,0 +1,36 @@
+import {DELETE_INFO, GET_INFO, GET_STOREDINFO} from "../actions/types"
+
+
+const initialState = {
+    infos: [],
+    info: {},  
+    
+};
+
+export default function(state = initialState, action){
+    switch(action.type){
+        case GET_INFO:
+        return {
+            ...state,
+            infos: action.payload
+        };
+
+        case GET_STOREDINFO:
+            return {
+                ...state,
+                info: action.payload
+            }
+
+        case DELETE_INFO:
+            return{
+                ...state,
+                infos: state.infos.filter(info => info.id !== action.payload )
+            }
+     
+      
+        default:
+            return state;
+    };
+
+        
+}
